04512abeb1f07d7a09cae3c784843383ec859fee
Nikhil Akki <nikhilravindra@gmail.com>
2017-12-27 21:20:57 +0530

    djanog webapp removed

--- webapp/src/amzwa/settings/production.py
+++
@@ -1,8 +0,0 @@
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&vu&fs!#wb$6!y9sadf2342y^^vg0(!r_kr!7behq+l_ii+*kwgemg^&)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = []
